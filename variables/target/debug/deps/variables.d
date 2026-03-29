0c6137a1494c617fcd99fb1fb9b5dd66fc379d28
YunFeng.CHEN <isYunFeng.CHEN@Gmail.com>
2026-03-29 14:17:24 +0800

    定义可变变量 mut

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\isYun\Desktop\RUST_learn\variables\target\debug\deps\variables.d: src\main.rs
+
+C:\Users\isYun\Desktop\RUST_learn\variables\target\debug\deps\variables.exe: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\isYun\Desktop\RUST_learn\variables\target\debug\variables.exe: C:\Users\isYun\Desktop\RUST_learn\variables\src\main.rs
